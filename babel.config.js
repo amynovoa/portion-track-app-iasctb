a941da611d8f92eb425e2c3aa8c57e0317de4688
natively.dev <api@natively.dev>
2025-11-28 18:54:20 +0000

    fix: Remove Natively editing system Babel plugins and context

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,6 @@
+
 module.exports = function (api) {
   api.cache(true);
-
-  const EDITABLE_COMPONENTS =
-    process.env.EXPO_PUBLIC_ENABLE_EDIT_MODE === "TRUE" &&
-    process.env.NODE_ENV === "development"
-      ? [
-          ["./babel-plugins/editable-elements.js", {}],
-          ["./babel-plugins/inject-source-location.js", {}],
-        ]
-      : [];
 
   return {
     presets: ["babel-preset-expo"],
@@ -38,7 +30,6 @@ module.exports = function (api) {
           },
         },
       ],
-      ...EDITABLE_COMPONENTS,
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-worklets/plugin", // react-native-worklets/plugin must be listed last!
     ],
